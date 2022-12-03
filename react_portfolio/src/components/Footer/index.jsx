a7f6e4de682fb2c5d5e45df86e5f309d311fd369
Karina Drummond <k.drummond528@gmail.com>
2022-12-03 14:09:46 -0800

    added footer index.jsx

---
+++ react_portfolio/src/components/Footer/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FaStackOverflow } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+
+        <div className='footer'>
+            <footer>
+                <ul>
+                    {/* github */}
+                    <li><a href="https://github.com/kdrummond528" target={'_blank'} title="Check out my GitHub!">
+                        <FaGithub size={"2em"} /></a>
+                    </li>
+
+                    {/* linkedin */}
+                    <li><a href="https://www.linkedin.com/in/karinadrummond" target={'_blank'} title="Connect with me on LinkedIn!">
+                        <FaLinkedinIn size={"2em"} /></a>
+                    </li>
+
+                    {/* stackoverflow */}
+                    <li>
+                        <a href="https://stackoverflow.com/users/20670765/karina-drummond" target={'_blank'} title='Check out my StackOverflow!'>
+                            <FaStackOverflow size={"2em"} /></a>
+                    </li>
+                </ul>
+            </footer>
+        </div >
+
+    );
+};
+
+export default Footer;
