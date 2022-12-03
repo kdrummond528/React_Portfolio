4bb726e63f971e03774bccdc3ce1dbf3862c3c4d
Karina Drummond <k.drummond528@gmail.com>
2022-12-03 14:08:52 -0800

    added about index.jsx

---
+++ react_portfolio/src/components/About/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About = () => {
+    return (
+
+        <div>
+
+            <h2 className='aboutpgtitle'>About Me</h2>
+
+            <img src="https://avatars.githubusercontent.com/u/48260505?v=4" className='profilepic' alt="Karina Drummond" />
+
+            <p className='aboutparagraph'>
+                I am a stay-at-home mom to four children, and wife to an active-duty Sailor.
+                A first-generation Dominican-American, born and raised in Miami. Before the Navy
+                relocated my family to San Diego in 2018, I lived in Orlando for 12 years and completed
+                a Bachelor of Science in Psychology in May of 2020, and a Master of Arts in Educational
+                Leadership in August of 2022 at UCF.
+                <br /><br />
+                I decided to enroll in the UCF Coding Bootcamp to fulfill the desire I have always had to
+                learn web development. As challenging as the program has been, I am thoroughly
+                enjoying every moment of it, and am looking forward to completing the program and beginning
+                my career in web development!
+            </p>
+
+        </div>
+
+    );
+};
+
+export default About;
