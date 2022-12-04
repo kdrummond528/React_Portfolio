b7b57326076644b10f32142c7fd050de29c13114
Karina Drummond <k.drummond528@gmail.com>
2022-12-04 10:08:30 -0800

    modified app.js

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-// import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-// import Header from "./components/Header";
-// import Nav from "./components/HeaderNav";
+import Nav from "./components/HeaderNav";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer";
+
+// import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 // const About = () => <div>About</div>;
 // const Portfolio = () => <div>Portfolio</div>;
@@ -18,60 +18,13 @@ import Footer from "./components/Footer";
 function App() {
 
   // starter code
-  // const [pageIndex, setPageIndex] = useState(0);
+  const [pageIndex, setPageIndex] = useState(0);
 
   return (
 
-    <div className=''>
-      {/* <Header> */}
-      <BrowserRouter>
-        <div className='nav-bar'>
-
-          <NavLink
-            to="about"
-            style={({ isActive }) => ({
-              color: isActive ? '#fff' : '#545e6f',
-              background: isActive ? '#7600dc' : '#f0f0f0',
-            })}>About Me
-          </NavLink>
-
-          <NavLink
-            to="about"
-            style={({ isActive }) => ({
-              color: isActive ? '#fff' : '#545e6f',
-              background: isActive ? '#7600dc' : '#f0f0f0',
-            })}>Portfolio
-          </NavLink>
-
-          <NavLink
-            to="about"
-            style={({ isActive }) => ({
-              color: isActive ? '#fff' : '#545e6f',
-              background: isActive ? '#7600dc' : '#f0f0f0',
-            })}>Contact
-          </NavLink>
-
-          <NavLink
-            to="about"
-            style={({ isActive }) => ({
-              color: isActive ? '#fff' : '#545e6f',
-              background: isActive ? '#7600dc' : '#f0f0f0',
-            })}>Resume
-          </NavLink>
-
-        </div>
-
-        <Routes>
-          <Route path='/about' element={<About />}></Route>
-          <Route path='/portfolio' element={<Portfolio />}></Route>
-          <Route path='/contact' element={<Contact />}></Route>
-          <Route path='/resume' element={<Resume />}></Route>
-        </Routes>
-
-      </BrowserRouter>
-      {/* </Header> */}
-
-      {/* <Nav
+    // starter code for nav bar
+    <div>
+      <Nav
         showAbout={() => setPageIndex(0)}
         showPortfolio={() => setPageIndex(1)}
         showContact={() => setPageIndex(2)}
@@ -87,35 +40,64 @@ function App() {
         <Resume />
       ) : (
         <About />
-      )} */}
+      )}
 
-      <Footer />
+      < Footer />
 
     </div >
 
-    // starter code
-    // <div>
-    //   <Nav
-    //     showAbout={() => setPageIndex(0)}
-    //     showPortfolio={() => setPageIndex(1)}
-    //     showContact={() => setPageIndex(2)}
-    //     showResume={() => setPageIndex(3)}
-    //   />
-    //   {pageIndex === 0 ? (
-    //     <About />
-    //   ) : pageIndex === 1 ? (
-    //     <Portfolio />
-    //   ) : pageIndex === 2 ? (
-    //     <Contact />
-    //   ) : pageIndex === 3 ? (
-    //     <Resume />
-    //   ) : (
-    //     <About />
-    //   )}
 
-    //   <Footer />
 
-    // </div>
+    // code to get nav bar links active for which pg youre currently on
+
+    // <div className=''>
+    //   {/* <Header> */}
+    //   <BrowserRouter>
+    //     <div className='nav-bar'>
+
+    //       <NavLink
+    //         to="about"
+    //         style={({ isActive }) => ({
+    //           color: isActive ? '#fff' : '#545e6f',
+    //           background: isActive ? '#7600dc' : '#f0f0f0',
+    //         })}>About Me
+    //       </NavLink>
+
+    //       <NavLink
+    //         to="about"
+    //         style={({ isActive }) => ({
+    //           color: isActive ? '#fff' : '#545e6f',
+    //           background: isActive ? '#7600dc' : '#f0f0f0',
+    //         })}>Portfolio
+    //       </NavLink>
+
+    //       <NavLink
+    //         to="about"
+    //         style={({ isActive }) => ({
+    //           color: isActive ? '#fff' : '#545e6f',
+    //           background: isActive ? '#7600dc' : '#f0f0f0',
+    //         })}>Contact
+    //       </NavLink>
+
+    //       <NavLink
+    //         to="about"
+    //         style={({ isActive }) => ({
+    //           color: isActive ? '#fff' : '#545e6f',
+    //           background: isActive ? '#7600dc' : '#f0f0f0',
+    //         })}>Resume
+    //       </NavLink>
+
+    //     </div>
+
+    //     <Routes>
+    //       <Route path='/about' element={<About />}></Route>
+    //       <Route path='/portfolio' element={<Portfolio />}></Route>
+    //       <Route path='/contact' element={<Contact />}></Route>
+    //       <Route path='/resume' element={<Resume />}></Route>
+    //     </Routes>
+
+    //   </BrowserRouter>
+    // </Header>
 
   );
 }
