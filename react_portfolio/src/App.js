38f2160425074268198eea451a4065243c1ebc19
Karina Drummond <k.drummond528@gmail.com>
2022-12-03 23:08:06 -0800

    modified headernav index.jsx and app.js

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,29 +1,43 @@
 import React, { useState } from 'react';
 import './App.css';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import Header from "./components/HeaderNav";
-import Nav from "./components/HeaderNav";
+// import Nav from "./components/HeaderNav";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 
+const About = () => <div>About</div>;
+const Portfolio = () => <div>Portfolio</div>;
+const Contact = () => <div>Contact</div>;
+const Resume = () => <div>Resume</div>;
+
 function App() {
 
-  const [pageIndex, setPageIndex] = useState(0);
+  // starter code
+  // const [pageIndex, setPageIndex] = useState(0);
 
   return (
-    <Header>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<About />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </Header>
 
+    <div className=''>
+      <Header>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/about' element={<Resume />}></Route>
+            <Route path='/resume' element={<Resume />}></Route>
+            <Route path='/resume' element={<Resume />}></Route>
+            <Route path='/resume' element={<Resume />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Header>
+
+      <Footer />
+
+    </div>
+
+    // starter code
     // <div>
     //   <Nav
     //     showAbout={() => setPageIndex(0)}
--- react_portfolio/src/components/HeaderNav/index.jsx
+++ react_portfolio/src/components/HeaderNav/index.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Nav = ({ showAbout, showPortfolio, showContact, showResume }) => {
     return (
-        // <div style={{ display: 'flex', color: 'white', justifyContent: 'center', alignItems: 'center' }}>
+
         <div>
             <header>
                 {/* my name in the header */}
                 <h1 className="nameheader">Karina Drummond</h1>
 
                 {/* nav bar links in header */}
-                <nav className='navbar'>
+                {/* <nav className='navbar'>
                     <ul>
                         <li className='navlinks' onClick={showAbout}>About Me</li>
                         <li className='navlinks' onClick={showPortfolio}>Portfolio</li>
                         <li className='navlinks' onClick={showContact}>Contact</li>
                         <li className='navlinks' onClick={showResume}>Resume</li>
                     </ul>
-                </nav>
+                </nav> */}
             </header>
 
         </div>
