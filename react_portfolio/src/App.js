8339baba3bf2abf92b6caa5fca1ffc0aa627ffd1
Karina Drummond <k.drummond528@gmail.com>
2022-12-03 21:06:04 -0800

    modified project index.jsx and app.js, added react router dom npm package

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from "./components/HeaderNav";
 import Nav from "./components/HeaderNav";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
@@ -12,28 +16,36 @@ function App() {
   const [pageIndex, setPageIndex] = useState(0);
 
   return (
-    <div>
-      <Nav
-        showAbout={() => setPageIndex(0)}
-        showPortfolio={() => setPageIndex(1)}
-        showContact={() => setPageIndex(2)}
-        showResume={() => setPageIndex(3)}
-      />
-      {pageIndex === 0 ? (
-        <About />
-      ) : pageIndex === 1 ? (
-        <Portfolio />
-      ) : pageIndex === 2 ? (
-        <Contact />
-      ) : pageIndex === 3 ? (
-        <Resume />
-      ) : (
-        <About />
-      )}
+    <Header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Header>
 
-      <Footer />
+    // <div>
+    //   <Nav
+    //     showAbout={() => setPageIndex(0)}
+    //     showPortfolio={() => setPageIndex(1)}
+    //     showContact={() => setPageIndex(2)}
+    //     showResume={() => setPageIndex(3)}
+    //   />
+    //   {pageIndex === 0 ? (
+    //     <About />
+    //   ) : pageIndex === 1 ? (
+    //     <Portfolio />
+    //   ) : pageIndex === 2 ? (
+    //     <Contact />
+    //   ) : pageIndex === 3 ? (
+    //     <Resume />
+    //   ) : (
+    //     <About />
+    //   )}
 
-    </div>
+    //   <Footer />
+
+    // </div>
 
   );
 }
