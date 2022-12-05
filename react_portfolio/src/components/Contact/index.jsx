630d72250567df4c95993c06ceb195fdd74d18da
Karina Drummond <k.drummond528@gmail.com>
2022-12-05 09:37:00 -0800

    modified contact.jsx and app.css

--- react_portfolio/src/components/Contact/index.jsx
+++ react_portfolio/src/components/Contact/index.jsx
@@ -1,94 +1,52 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 // import { formValues, isSubmit } from 'react-hooks';
 import { FaMobileAlt } from 'react-icons/fa';
 import { TfiEmail } from 'react-icons/tfi';
 import { SiWhatsapp } from 'react-icons/si';
 
 const Contact = () => {
-    const initialValues = { name: "", email: "", message: "" };
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value })
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setFormErrors(validate(formValues));
-        setIsSubmit(true);
-    };
-
-    useEffect(() => {
-        console.log(formErrors);
-        if (Object.keys(formErrors).length === 0 && isSubmit) {
-            console.log(formValues);
+    const [formStatus, setFormStatus] = React.useState('Send')
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setFormStatus('Sending...')
+        const { name, email, message } = e.target.elements
+        let conFom = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
         }
-    }, [formErrors]);
-
-    const validate = (values) => {
-        const errors = {};
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        if (!values.name) {
-            errors.name = "Name is required."
-        }
-        if (!values.email) {
-            errors.email = "Email is required."
-        } else if (!regex.test(values.email)) {
-            errors.email = "Enter a valid email.";
-        }
-        if (!values.message) {
-            errors.message = "Message is required."
-        }
-        return errors;
-    };
+        console.log(conFom)
+    }
 
     return (
 
-        < div >
-            {Object.keys(formErrors).length === 0 && isSubmit ? (
-                <div className='MsgSuccess'>Message sent successfully!</div>
-            ) : (
-                <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-            )};
-
+        <div>
+            {/* page title */}
             <h2 className='contactpgtitle'>Contact</h2>
-            <form className='contactform' onSubmit={handleSubmit}>
 
+            {/* contact form */}
+            <form className='contactform' onSubmit={onSubmit}>
                 {/* name input */}
                 <div>
-                    <label className='contactinputtitle'>Name:</label>
-                    <input type="text" className='nameinput' placeholder='John Doe'
-                        value={formValues.name} onChange={handleChange} />
+                    <label className='contactinputtitle' htmlFor="name">Name</label>
+                    <input className='nameinput' type="text" id="name" required />
                 </div>
-                {/* name error message */}
-                <p className='errormsg'>{formErrors.name}</p>
-
                 {/* email input */}
                 <div>
-                    <label className='contactinputtitle'>Email address:</label>
-                    <input type="text" className='emailinput' placeholder='johndoe@gmail.com'
-                        value={formValues.email} onChange={handleChange} />
+                    <label className='contactinputtitle' htmlFor="email">Email</label>
+                    <input className='emailinput' type="email" id="email" required />
                 </div>
-                {/* email error message */}
-                <p className='errormsg'>{formErrors.email}</p>
-
                 {/* message input */}
                 <div>
-                    <label className='contactinputtitle'>Message:</label>
-                    <input type="text" className='messageinput' placeholder='Hello...'
-                        value={formValues.message} onChange={handleChange} />
+                    <label className='contactinputtitle' htmlFor="message">Message</label>
+                    <textarea className='messageinput' id="message" required />
                 </div>
-                {/* message error message */}
-                <p className='errormsg'>{formErrors.message}</p>
-
                 {/* submit button */}
-                <div>
-                    <button className='contactform-submit-btn'>Submit</button>
-                </div>
+                <button className='contactform-submit-btn' type="submit">
+                    {formStatus}
+                </button>
+
             </form>
 
             {/* container with direct contact information */}
@@ -110,6 +68,6 @@ const Contact = () => {
         </div >
 
     );
-};
+}
 
 export default Contact;
