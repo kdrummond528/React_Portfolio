726163ab364413a7e53b8831531c89627cf21d99
Karina Drummond <k.drummond528@gmail.com>
2022-12-06 12:12:28 -0800

    modified about index.jsx

--- react_portfolio/src/components/About/index.jsx
+++ react_portfolio/src/components/About/index.jsx
@@ -12,9 +12,9 @@ const About = () => {
             <p className='aboutparagraph'>
                 I am a stay-at-home mom to four children, and wife to an active-duty Sailor.
                 A first-generation Dominican-American, born and raised in Miami. Before the Navy
-                relocated my family to San Diego in 2018, I lived in Orlando for 12 years and completed
-                a Bachelor of Science in Psychology in May of 2020, and a Master of Arts in Educational
-                Leadership in August of 2022 at UCF.
+                relocated my family to San Diego in 2018, I lived in Orlando for 12 years and began
+                studying at UCF. I successfully completed a Bachelor of Science in Psychology in May
+                of 2020, and a Master of Arts in Educational Leadership in August of 2022 at UCF.
                 <br /><br />
                 I decided to enroll in the UCF Coding Bootcamp to fulfill the desire I have always had to
                 learn web development. As challenging as the program has been, I am thoroughly
