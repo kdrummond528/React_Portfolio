6a9ea82731fd2ea84cd4f77e26864a00bc3233d3
Karina Drummond <k.drummond528@gmail.com>
2022-12-05 21:05:39 -0800

    modified app.css, index.js, installed react-router-dom, modified headernav

--- react_portfolio/src/components/HeaderNav/index.jsx
+++ react_portfolio/src/components/HeaderNav/index.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 
+
 const HeaderNav = ({ showAbout, showPortfolio, showContact, showResume }) => {
-
-    // const nav = document.getElementById("navbar");
-
-    // var lis = nav.getElementsByClassName("navlinks");
-    // for (var i = 0; i < lis.length; i++) {
-    //     lis[i].addEventListener("click", function () {
-    //         var current = document.getElementsByClassName("active");
-    //         current[0].className = current[0].className.replace(" active", "");
-    //         this.className += " active";
-    //     });
 
     return (
 
@@ -22,16 +13,60 @@ const HeaderNav = ({ showAbout, showPortfolio, showContact, showResume }) => {
                 {/* nav bar links in header */}
                 <nav className='navbar'>
                     <ul>
-                        <li className='navlinks' onClick={showAbout}>About Me</li>
-                        <li className='navlinks' onClick={showPortfolio}>Portfolio</li>
-                        <li className='navlinks' onClick={showContact}>Contact</li>
-                        <li className='navlinks' onClick={showResume}>Resume</li>
+                        <li className='navlink' onClick={showAbout}>About Me</li>
+                        <li className='navlink' onClick={showPortfolio}>Portfolio</li>
+                        <li className='navlink' onClick={showContact}>Contact</li>
+                        <li className='navlink' onClick={showResume}>Resume</li>
                     </ul>
                 </nav>
             </header>
 
         </div>
+
     );
 };
 
 export default HeaderNav;
+
+
+
+// const HeaderNav = ({ pageIndex, showAbout, showPortfolio, showContact, showResume }) => {
+
+//     return (
+
+//         <div>
+//             <header>
+//                 {/* my name in the header */}
+//                 <h1 className="nameheader">Karina Drummond</h1>
+
+//                 {/* nav bar links in header */}
+//                 <nav className='navbar'>
+//                     <ul>
+
+//                         {/* link to about pg */}
+//                         <li className='navlink'>
+//                             <p onClick={showAbout} activeClassName='activenavlink' >
+//                                 About Me</p>
+//                         </li>
+
+//                         {/* link to portfolio pg */}
+//                         <li className='navlink'>
+//                             <p onClick={showPortfolio} className={pageIndex === '1' ? 'navlink active' : 'navlink'}>
+//                                 Portfolio</p>
+//                         </li>
+//                         {/* link to contact pg */}
+//                         <li className='navlink'>
+//                             <p onClick={showContact} className={pageIndex === '2' ? 'navlink active' : 'navlink'}>
+//                                 Contact</p>
+//                         </li>
+//                         {/* link to resume pg */}
+//                         <li className='navlink'>
+//                             <p href="#resume" onClick={showResume}
+//                                 className={pageIndex === '3' ? 'navlink active' : 'navlink'}>
+//                                 Resume</p>
+//                         </li>
+//                     </ul>
+//                 </nav>
+
+//             </header>
+//         </div>
