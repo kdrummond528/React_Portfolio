2e9781e0fa1f3fed19327354b11c81a56952c6b5
Karina Drummond <k.drummond528@gmail.com>
2022-12-09 12:32:05 -0800

    modified poroject index.jsx and app.css

--- react_portfolio/src/components/Project/index.jsx
+++ react_portfolio/src/components/Project/index.jsx
@@ -11,32 +11,36 @@ export default function Project(element) {
                 onMouseOver={() => setIsShown(true)}
                 onMouseOut={() => setIsShown(false)}>
                 <img src={element.img} alt={element.name}
-                    className='projectcardimg' />
+                    className='projectcardimg' style={isShown ? { filter: "opacity(20%)" } : {}} />
             </div>
-            {isShown && (
+            {
+                isShown ? (
 
-                <div className='hoverprojectinfo'>
+                    <div className='hoverprojectinfo'
+                        onMouseOver={() => setIsShown(true)}
+                        onMouseOut={() => setIsShown(false)}>
 
-                    <h2 className='projectlinks'>
+                        <h2 className='projectlinks'>
 
-                        {/* deployed application link */}
+                            {/* deployed application link */}
 
-                        <a href={element.link} target="_blank" rel="noreferrer" className="deployedprojectlink">
-                            {element.name}</a>
+                            <a href={element.link} target="_blank" rel="noreferrer" className="deployedprojectlink">
+                                {element.name}</a>
 
-                        {/* github repo link */}
+                            {/* github repo link */}
 
-                        <a href={element.repo} target="_blank" rel="noreferrer" className="githubrepoicon">
-                            <FaGithub /></a>
-                    </h2>
+                            <a href={element.repo} target="_blank" rel="noreferrer" className="githubrepoicon">
+                                <FaGithub /></a>
+                        </h2>
 
-                    {/* project description and info */}
-                    <p className="projectinfo">{element.id}</p>
+                        {/* project description and info */}
+                        <p className="projectinfo">{element.id}</p>
 
-                </div>
-            )}
+                    </div>
+                ) : ""
+            }
 
-        </div>
+        </div >
 
     );
 };
